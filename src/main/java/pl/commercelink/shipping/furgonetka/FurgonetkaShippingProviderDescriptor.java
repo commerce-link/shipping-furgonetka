89c0f3a4645b4da862a087244bfdd6f0ff713f27
ptrepka <przemyslaw.trepka@pragmaticcoders.com>
2026-05-14 18:04:54 +0200

    Bump up package version

--- src/main/java/pl/commercelink/shipping/furgonetka/FurgonetkaShippingProviderDescriptor.java
+++ src/main/java/pl/commercelink/shipping/furgonetka/FurgonetkaShippingProviderDescriptor.java
@@ -1,5 +1,6 @@
 package pl.commercelink.shipping.furgonetka;
 
+import pl.commercelink.provider.api.AuthConfig;
 import pl.commercelink.provider.api.ProviderField;
 import pl.commercelink.rest.client.RestApiWithRetry;
 import pl.commercelink.shipping.api.ShippingProvider;
@@ -13,6 +14,8 @@ import static pl.commercelink.provider.api.ProviderField.FieldType.TEXT;
 
 public class FurgonetkaShippingProviderDescriptor implements ShippingProviderDescriptor {
 
+    private static final String DEFAULT_API_URL = "https://api.furgonetka.pl";
+
     @Override
     public String name() {
         return "furgonetka";
@@ -24,25 +27,23 @@ public class FurgonetkaShippingProviderDescriptor implements ShippingProviderDes
     }
 
     @Override
-    public Map<String, String> metadata() {
-        return Map.of(
-                "authType", "oauth2",
-                "authEndpointPath", "/oauth/token",
-                "refreshEndpointPath", "/oauth/token",
-                "refreshTokenExpirationSeconds", String.valueOf(29L * 24 * 60 * 60),
-                "acceptHeader", "application/vnd.furgonetka.v1+json"
-        );
+    public AuthConfig authConfig() {
+        return new AuthConfig.OAuth2(
+                DEFAULT_API_URL,
+                "/oauth/token",
+                "/oauth/token",
+                29L * 24 * 60 * 60,
+                "application/vnd.furgonetka.v1+json");
     }
 
     @Override
     public List<ProviderField> configurationFields() {
         return List.of(
-                new ProviderField("apiUrl", "API URL", TEXT, true, "https://api.furgonetka.pl"),
+                new ProviderField("apiUrl", "API URL", TEXT, true, DEFAULT_API_URL),
                 new ProviderField("username", "API Username", TEXT, true, ""),
                 new ProviderField("password", "API Password", PASSWORD, true, ""),
                 new ProviderField("clientId", "Client ID", TEXT, true, ""),
-                new ProviderField("clientSecret", "Client Secret", PASSWORD, true, "")
-        );
+                new ProviderField("clientSecret", "Client Secret", PASSWORD, true, ""));
     }
 
     @Override
